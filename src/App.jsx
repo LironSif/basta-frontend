a5f922c1c9bf514936ba66baa72c290eca0891cc
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2024-01-14 20:27:03 +0200

    adding routes and creating homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 //import { ThemeProvider } from "@mui/system"
 import { Box, Container, Typography } from "@mui/material";
 import Navbar from "./Components/Navbar";
-// import SellerShop from "./Page/SellerShop/SellerShop";
+import SellerShop from "./Page/SellerShop/SellerShop";
 // import Preview from "./Page/Preview/Preview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Page/Homepage/Homepage";
-import ShopsPage from "./Page/ShopsPage/ShopsPage";
+// import ShopsPage from "./Page/ShopsPage/ShopsPage";
 import { useState } from "react";
 import { NavigateLocation } from "./Components/NavigateLocation";
 
@@ -19,8 +19,8 @@ function App() {
         <Routes>
           {!isClicked && <Route path="/" element={<Homepage setIsClicked={setIsClicked}/>} />}
           {/* <Route path="/preview" element={<Preview />} /> */}
-          {/* <Route path="/sellerList" element={<SellerShop />} /> */}
-          <Route path="/shopsList" element={<ShopsPage />} />
+          <Route path="/sellerList" element={<SellerShop />} />
+          {/* <Route path="/shopsList" element={<ShopsPage />} /> */}
         </Routes>
       </BrowserRouter>
     </>
